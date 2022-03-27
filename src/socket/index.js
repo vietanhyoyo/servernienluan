a99a043c8c593adaa7123208818774d95baba9b3
vietanhyoyo <vanhyoyo@gmail.com>
2022-03-27 21:11:06 +0700

    chat khach hang den nhan vien

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,13 +1,15 @@
 
 
-function socket(io){
+function socket(io) {
     /**io lắng nghe khi có connect đến */
     io.on('connection', (socket) => {
         console.log('----------- Đã kết nối socket ----------------------');
         socket.on('on-chat', data => {
             console.log(data);
-            if(data.role === null)
+            if (data.role === null) {
                 io.emit('user-chat', data);
+                io.emit(data.id, data);
+            }
         })
     })
 }
